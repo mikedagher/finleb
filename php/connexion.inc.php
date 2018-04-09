e23b0c8e1fdf7e45cd84e7fb8502566b54fc5d31
mike dagher <mike-dagher@hotmail.com>
2018-04-09 17:27:39 +0300

    mike

--- php/connexion.inc.php
+++ php/connexion.inc.php
@@ -4,10 +4,6 @@ function connex() {
 	
 	$idcon=mysqli_connect("localhost", "root", "root", "finleb");
 	
-	if ($idcon)
-		echo "connexion reussie";
-	else
-		echo "connexion impossible...";
 	return $idcon;
 }
 ?>

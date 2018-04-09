28840c58c778fd0e4cb0ac2601f5bf8e35a73fd2
AnthonyBechay <33066619+AnthonyBechay@users.noreply.github.com>
2018-04-09 18:05:51 +0300

    f

--- php/connexion.inc.php
+++ php/connexion.inc.php
@@ -4,10 +4,6 @@ function connex() {
 	
 	$idcon=mysqli_connect("localhost", "root", "root", "finleb");
 	
-<<<<<<< HEAD
-	
-=======
->>>>>>> 791f7a67a068d79e4980540002b0e60d7e867dd3
 	return $idcon;
 }
 ?>

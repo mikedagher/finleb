420c842cc1fcf847392d8ce4416bfc6f026acab2
AnthonyBechay <33066619+AnthonyBechay@users.noreply.github.com>
2018-04-10 14:37:27 +0300

    f

--- jobs_list.php
+++ jobs_list.php
@@ -36,24 +36,24 @@ echo "$hi1";
      
 
 
-<div class="card text-center" style="max-width: 202px;">
+            <div class="card text-center" style="max-width: 202px;">
 
-<!-- Heading -->
-<div class="card-body">
-<h4 class="card-title">Sun Gone</h4>
-<h6 class="card-subtitle text-muted">Sun disappears!</h6>
-</div>
+            <!-- Heading -->
+            <div class="card-body">
+            <h4 class="card-title">Sun Gone</h4>
+            <h6 class="card-subtitle text-muted">Sun disappears!</h6>
+            </div>
 
-<!-- Image -->
-<img src="/pix/samples/12s.jpg" alt="Photo of sunset">
+            <!-- Image -->
+            <img src="/pix/samples/12s.jpg" alt="Photo of sunset">
 
-<!-- Text Content -->
-<div class="card-body">
-<p class="card-text">The sun disappeared behind the horizon today. Experts claim it could be gone for good!</p>
-<a href="#" class="btn btn-primary">Full Report</a>
-</div>
+            <!-- Text Content -->
+            <div class="card-body">
+            <p class="card-text">The sun disappeared behind the horizon today. Experts claim it could be gone for good!</p>
+            <a href="#" class="btn btn-primary">Full Report</a>
+            </div>
 
-</div>
+            </div>
 
 
     </div>
@@ -62,24 +62,24 @@ echo "$hi1";
 
 
 
-<div class="card text-center" style="max-width: 202px;">
+            <div class="card text-center" style="max-width: 202px;">
 
-<!-- Heading -->
-<div class="card-body">
-<h4 class="card-title">Sun Gone</h4>
-<h6 class="card-subtitle text-muted">Sun disappears!</h6>
-</div>
+            <!-- Heading -->
+            <div class="card-body">
+            <h4 class="card-title">Sun Gone</h4>
+            <h6 class="card-subtitle text-muted">Sun disappears!</h6>
+            </div>
 
-<!-- Image -->
-<img src="/pix/samples/12s.jpg" alt="Photo of sunset">
+            <!-- Image -->
+            <img src="/pix/samples/12s.jpg" alt="Photo of sunset">
 
-<!-- Text Content -->
-<div class="card-body">
-<p class="card-text">The sun disappeared behind the horizon today. Experts claim it could be gone for good!</p>
-<a href="#" class="btn btn-primary">Full Report</a>
-</div>
+            <!-- Text Content -->
+            <div class="card-body">
+            <p class="card-text">The sun disappeared behind the horizon today. Experts claim it could be gone for good!</p>
+            <a href="#" class="btn btn-primary">Full Report</a>
+            </div>
 
-</div>
+            </div>
 
 
 </div>
@@ -87,32 +87,142 @@ echo "$hi1";
 
 
 
-<div class="card text-center" style="max-width: 202px;">
+            <div class="card text-center" style="max-width: 202px;">
 
-<!-- Heading -->
-<div class="card-body">
-<h4 class="card-title">Sun Gone</h4>
-<h6 class="card-subtitle text-muted">Sun disappears!</h6>
-</div>
+            <!-- Heading -->
+            <div class="card-body">
+            <h4 class="card-title">Sun Gone</h4>
+            <h6 class="card-subtitle text-muted">Sun disappears!</h6>
+            </div>
 
-<!-- Image -->
-<img src="/pix/samples/12s.jpg" alt="Photo of sunset">
+            <!-- Image -->
+            <img src="/pix/samples/12s.jpg" alt="Photo of sunset">
 
-<!-- Text Content -->
-<div class="card-body">
-<p class="card-text">The sun disappeared behind the horizon today. Experts claim it could be gone for good!</p>
-<a href="#" class="btn btn-primary">Full Report</a>
-</div>
+            <!-- Text Content -->
+            <div class="card-body">
+            <p class="card-text">The sun disappeared behind the horizon today. Experts claim it could be gone for good!</p>
+            <a href="#" class="btn btn-primary">Full Report</a>
+            </div>
 
-</div>
-
-
-
-
+            </div>
 
 </div>
   </div>
 </div>
+
+
+
+    <link rel="stylesheet" type="text/css" media="screen" href="css/jobscarousel.css" />
+
+<section class="container p-t-3">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1>Bootstrap 4 Card Slider</h1>
+        </div>
+    </div>
+</section>
+<section class="carousel slide" data-ride="carousel" id="postsCarousel">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 text-md-right lead">
+                <a class="btn btn-outline-secondary prev" href="" title="go back"><i class="fa fa-lg fa-chevron-left"></i></a>
+                <a class="btn btn-outline-secondary next" href="" title="more"><i class="fa fa-lg fa-chevron-right"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="container p-t-0 m-t-2 carousel-inner">
+        <div class="row row-equal carousel-item active m-t-0">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-img-top card-img-top-250">
+                        <img class="img-fluid" src="http://i.imgur.com/EW5FgJM.png" alt="Carousel 1">
+                    </div>
+                    <div class="card-block p-t-2">
+                        <h6 class="small text-wide p-b-2">Insight</h6>
+                        <h2>
+                            <a href>Why Stuff Happens Every Year.</a>
+                        </h2>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-img-top card-img-top-250">
+                        <img class="img-fluid" src="http://i.imgur.com/Hw7sWGU.png" alt="Carousel 2">
+                    </div>
+                    <div class="card-block p-t-2">
+                        <h6 class="small text-wide p-b-2">Development</h6>
+                        <h2>
+                            <a href>How to Make Every Line Count.</a>
+                        </h2>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-img-top card-img-top-250">
+                        <img class="img-fluid" src="http://i.imgur.com/g27lAMl.png" alt="Carousel 3">
+                    </div>
+                    <div class="card-block p-t-2">
+                        <h6 class="small text-wide p-b-2">Design</h6>
+                        <h2>
+                            <a href>Responsive is Essential.</a>
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row row-equal carousel-item m-t-0">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-img-top card-img-top-250">
+                        <img class="img-fluid" src="//visualhunt.com/photos/l/1/office-student-work-study.jpg" alt="Carousel 4">
+                    </div>
+                    <div class="card-block p-t-2">
+                        <h6 class="small text-wide p-b-2">Another</h6>
+                        <h2>
+                            <a href>Tagline or Call-to-action.</a>
+                        </h2>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-img-top card-img-top-250">
+                        <img class="img-fluid" src="//visualhunt.com/photos/l/1/working-woman-technology-computer.jpg" alt="Carousel 5">
+                    </div>
+                    <div class="card-block p-t-2">
+                        <h6 class="small text-wide p-b-2"><span class="pull-xs-right">12.04</span> Category 1</h6>
+                        <h2>
+                            <a href>This is a Blog Title.</a>
+                        </h2>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 fadeIn wow">
+                <div class="card">
+                    <div class="card-img-top card-img-top-250">
+                        <img class="img-fluid" src="//visualhunt.com/photos/l/1/people-office-team-collaboration.jpg" alt="Carousel 6">
+                    </div>
+                    <div class="card-block p-t-2">
+                        <h6 class="small text-wide p-b-2">Category 3</h6>
+                        <h2>
+                            <a href>Catchy Title of a Blog Post.</a>
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
 
 
     <script src="vendor/jquery/jquery.min.js"></script>
@@ -120,7 +230,7 @@ echo "$hi1";
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-   <script src="vendor/bootstrap/js/ourjs.js"></script>
+   <script src="javascript/scripts.js"></script>
 </body>
 
 
